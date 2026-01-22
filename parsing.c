d546d8e461df9f8eb18e88c2732f9c5e84f12c89
gonmpr <gmporrorosas@gmail.com>
2026-01-22 01:01:23 -0300

    evaluate

--- parsing.c
+++ parsing.c
@@ -34,25 +34,36 @@ void add_history(char *unused){}
 // cc -std=c99 -Wall parsing.c mpc.c -ledit -lm -o parsing
 
 
+long eval_op(long x, char *op, long y);
+long eval(mpc_ast_t *t);
 
 int main(int argc, char** argv) {
+
+  mpc_parser_t* Lint = mpc_new("int");
+  mpc_parser_t* Lfloat = mpc_new("float");
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr = mpc_new("expr");
   mpc_parser_t* MyLisp = mpc_new("mylisp");
 
   mpca_lang(MPCA_LANG_DEFAULT,
-      "                                                   \
-        number  : /-?[0-9]+/ ;                            \
-        operator: '+' | '-' | '*' | '/';                  \
-        expr    : <number> | '(' <operator> <expr>+ ')';  \
-        mylisp  : /^/ <operator> <expr>+ /$/ ;            \
+      "                                                                    \
+        int     : /-?[0-9]+/ ;                                             \
+        float   : /-?[0-9]+\\.[0-9]+/ ;                                    \
+        number  : <float> | <int> ;                                        \
+        operator: '+' | \"add\" | '-' | \"sub\" |                          \
+                  '*' | \"mul\" | '/' | \"div\" |                          \
+                  '%' | \"res\" ;                                          \
+        expr    : <number> | '(' <operator> <expr>+ ')';                   \
+        mylisp  : /^/ <operator> <expr>+ /$/ ;                             \
       ",
-      Number, Operator, Expr, MyLisp);
+      Lint, Lfloat, Number, Operator, Expr, MyLisp);
 
 
   puts("My-little-lisp Line Interpreter");
   puts("Press Ctrl+c to Exit\n");
+
+
 
   while (1) {
 
@@ -66,9 +77,12 @@ int main(int argc, char** argv) {
     /* Attempt to Parse the user Input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, MyLisp, &r)) {
-      /* On Success Print the AST */
-      mpc_ast_print(r.output);
+
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
+
+      
     } else {
       /* Otherwise Print the Error */
       mpc_err_print(r.error);
@@ -78,6 +92,37 @@ int main(int argc, char** argv) {
     free(input);
   }
 
-  mpc_cleanup(4, Number, Operator, Expr, MyLisp);
+  mpc_cleanup(6, Lint, Lfloat, Number, Operator, Expr, MyLisp);
+  return 0;
+}
+
+
+
+long eval(mpc_ast_t *t){
+
+  if (strstr(t->tag, "number")){
+    return atoi(t->contents);
+  }
+  
+  char *op = t->children[1]->contents;
+  
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr")){
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+
+long eval_op(long x, char *op, long y) {
+  if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) { return x / y; }
+  if (strcmp(op, "%") == 0 || strcmp(op, "res") == 0) { return x % y; }
   return 0;
 }
